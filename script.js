cb3fa60b368ca1cbefe5bb109d2e5fae93fc803b
Hanka Celá <hana-cela@email.cz>
2024-10-07 18:39:54 +0200

    Pridavam nove soubory 3

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+/*
 const hodinovka = 300
 const pocetHodin = promt("Zadej pocet hodin zaměstnance", "160")
 const premie = promt("Zadej mimoradnou odmenu v Kc")
 const plat = hodinovka * pocetHodin
 
 document.body.innerHTML = "<p>Pepa Novak si vydelal: " + plat + " Kc.<p>"
+*/ 
+
+const hodinovka = 300
+const pocetHodin = Number(prompt("Zadej pocet hodin zamestnance"))
+const premie = Number(prompt("Zadej mimoradnou odmenu v Kc")) // "10000" -> 10000
+const plat = (hodinovka * pocetHodin) + premie
+
+document.body.innerHTML = "<p>Pepa Novak si vydelal: " + plat + " Kc.</p>"
