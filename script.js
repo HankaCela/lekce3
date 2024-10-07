9c872749fd7b46b3c7d38612ab4e68af395366a7
Hanka Celá <hana-cela@email.cz>
2024-10-07 19:09:22 +0200

    uprava souboru 5

--- script.js
+++ script.js
@@ -16,21 +16,21 @@ document.body.innerHTML = "<p>Pepa Novak si vydelal: " + plat + " Kc.</p>"
 
 
 //Očkování
-        const jmeno = prompt("Zadejte vaše jméno:");
-        const vek = Number(prompt("Zadejte váš věk:"));
+const jmeno = prompt("Zadejte vaše jméno:");
+const vek = Number(prompt("Zadejte váš věk:"));
 
         
-        document.body.innerHTML = `
+document.body.innerHTML = `
             <h1>Registrace na očkování</h1>
             <p>${jmeno}, věk: ${vek}</p>`;
 
 //Výplata jako stránka
-        const hodinovaSazba = Number(prompt("Zadejte vaši hodinovou sazbu (v Kč):"));
-        const pocetHodinDenne = Number(prompt("Zadejte počet hodin, které pracujete denně:"));
-        const pocetDniVmesici = Number(prompt("Zadejte počet pracovních dní v měsíci:"));
-        const hrubaMzda = hodinovaSazba * pocetHodinDenne * pocetDniVmesici;
+const hodinovaSazba = Number(prompt("Zadejte vaši hodinovou sazbu (v Kč):"));
+const pocetHodinDenne = Number(prompt("Zadejte počet hodin, které pracujete denně:"));
+const pocetDniVmesici = Number(prompt("Zadejte počet pracovních dní v měsíci:"));
+const hrubaMzda = hodinovaSazba * pocetHodinDenne * pocetDniVmesici;
 
-        document.body.innerHTML = `
-            <h1>Výpočet vaší hrubé mzdy</h1>
-            <p>Pokud máte hodinovou sazbu ${hodinovaSazba} Kč, pracujete ${pocetHodinDenne} hodin denně po dobu ${pocetDniVmesici} dní v měsíci, vaše hrubá mzda bude ${hrubaMzda} Kč.</p>
+document.body.innerHTML = `
+    <h1>Výpočet vaší hrubé mzdy</h1>
+    <p>Pokud máte hodinovou sazbu ${hodinovaSazba} Kč, pracujete ${pocetHodinDenne} hodin denně po dobu ${pocetDniVmesici} dní v měsíci, vaše hrubá mzda bude ${hrubaMzda} Kč.</p>
         `;
